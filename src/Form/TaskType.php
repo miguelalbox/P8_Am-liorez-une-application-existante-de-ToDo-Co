2e9f7b1017531d320ae3332ec5099cc087004df6
miguelalbox <49124664+miguelalbox@users.noreply.github.com>
2022-10-13 20:36:04 +0200

    [Features] Mis a jour de Symfony 3 a Symfony 6 #1

---
+++ src/Form/TaskType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+                ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu'
+                ])
+            ->add('isDone', CheckboxType::class, [
+                'label' => 'Terminé'
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
