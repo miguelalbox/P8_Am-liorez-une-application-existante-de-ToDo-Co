9d9c145236cc7d1176f74e5604cb8ae6a3a3306e
miguelalbox <49124664+miguelalbox@users.noreply.github.com>
2022-10-19 12:35:26 +0200

    [Features] Fixtures created #2

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+
+        for($i = 0; $i < 20; $i++){
+        
+            $task = new Task();
+            $task->setTitle('Title' . $i);
+            $task->setContent('Content' . $i);
+            $task->setIsDone(false);
+            $task->setCreatedAt((new \DateTimeImmutable('now')));
+            $manager->persist($task);
+        }
+
+        for($i = 0; $i < 10; $i++){
+            $user = new User;
+            $user->setUsername('user' . $i);
+            $user->setEmail('email' . $i . '@gmail.com');
+            $user->setPassword(password_hash('123', PASSWORD_DEFAULT));
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+        }
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
